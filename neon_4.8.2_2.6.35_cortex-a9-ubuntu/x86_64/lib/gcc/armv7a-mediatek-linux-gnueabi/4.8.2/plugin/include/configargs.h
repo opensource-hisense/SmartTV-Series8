4dd524a6b517ee9fd8d76816ab99d3c7aba7bc68
opensource-hisense <opensource@hisense.com>
2018-03-22 06:35:58 +0800

    Add ToolChain

---
+++ neon_4.8.2_2.6.35_cortex-a9-ubuntu/x86_64/lib/gcc/armv7a-mediatek-linux-gnueabi/4.8.2/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-4.8.2/configure --prefix=/tmp/root/usr/toolchain-4.8.2-neon/cortex-a9/gcc --host=x86_64-pc-linux-gnu --build=x86_64-pc-linux-gnu --target=armv7a-mediatek-linux-gnueabi --with-sysroot=/tmp/root/usr/toolchain-4.8.2-neon/cortex-a9/gcc/sysroot --with-arch=armv7-a --with-tune=cortex-a9 --with-cpu=cortex-a9 --with-interwork --with-fpu=neon-vfpv4 --with-float=softfp --with-gnu-as --with-gnu-ld --disable-nls --enable-shared --enable-__cxa_atexit --disable-multilib --enable-c99 --enable-long-long --enable-threads=posix --enable-languages=c,c++ --with-gmp=/tmp/root/build/x86_64 --with-mpfr=/tmp/root/build/x86_64 --with-cloog=/tmp/root/build/x86_64 --with-isl=/tmp/root/build/x86_64 --with-libelf=/tmp/root/build/x86_64 --program-transform-name='s,^,armv7a-mediatek482_001_neon-linux-gnueabi-,' --with-mpc=/tmp/root/build/x86_64 --enable-lto --without-system-libunwind --disable-rpath --with-host-libstdcxx='-static-libgcc -Wl,-Bstatic,-lstdc++,-Bdynamic,-lm' --with-specs='%{!fno-unwind-tables:-funwind-tables}' --with-build-time-tools=/tmp/root/usr/toolchain-4.8.2-neon/cortex-a9/binutils/armv7a-mediatek-linux-gnueabi/bin/ --enable-cxx-flags='-g -O2'";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "cortex-a9" }, { "arch", "armv7-a" }, { "tune", "cortex-a9" }, { "float", "softfp" }, { "fpu", "neon-vfpv4" }, { "tls", "gnu" } };
